c86af8a093afa76928ed4e0f183115cb2d85a9e9
ronavnanda <ronav.nanda@gmail.com>
2026-09-08 11:16:56 -0700

    Simplify output statement in print.cpp
    Simplify std::cout by adding namespace

--- print.cpp
+++ print.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
+//this line makes it so that u can take the std out from std::cout<<"..."; ==> count<<"...";
+using namespace std;
+
 int main() {
-    std::cout<<"Hello Citadel!";
+    cout<<"Hello Citadel!";
     return 0;
 }
